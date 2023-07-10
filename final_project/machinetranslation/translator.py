9d321d0f23bb1323035a51aca4d1755ad330890e
bmt-dev <bmtdev11@gmail.com>
2023-07-10 11:37:46 -0400

    Final changes

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,14 @@
+'''Translator module'''
+
+from deep_translator import MyMemoryTranslator
+
+def english_to_french(english_text):
+    '''Translates english to french'''
+    french_text = MyMemoryTranslator(source='en-US', target='fr-FR').translate(english_text)
+    return french_text
+
+
+def french_to_english(french_text):
+    '''Translates french to english'''
+    english_text = MyMemoryTranslator(source='fr-FR', target='en-US').translate(french_text)
+    return english_text
